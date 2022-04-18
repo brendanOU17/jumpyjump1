ce50cc044604edcf867744fcd055dd93b77ab7be
brendanou <0132735@kdu-online.com>
2022-04-18 17:06:29 +0800

    added Scripts

--- Jumpy/Assets/Scripts/Player/PlayerController.cs
+++ Jumpy/Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float movementSpeed = 10f;
+    public Rigidbody2D rb;
+    public float xMovement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -13,6 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movementSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = xMovement;
+        rb.velocity = velocity;
     }
 }
